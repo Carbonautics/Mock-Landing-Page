f5b5827659752fbde1e4ec9f554003c4ef3218c0
Pratik Nadgouda <pratiknadgouda@pm.me>
2021-09-02 13:14:08 +0530

    added mobile menu
    Added the mobile menu with animations and content....

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+const btnHam  = document.getElementById('btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHam.addEventListener('click', function(){
+  console.log('open ham');
+  if(header.classList.contains('open')){ //Close hamburger menu
+    body.classList.remove('no-scroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
+  }else { // open hamburger menu
+    body.classList.add('no-scroll')
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
+
+  }
+});
